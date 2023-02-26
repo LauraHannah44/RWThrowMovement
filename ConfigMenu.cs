44d2b3ee5e6d1850dbfb75fbfa91efd226aba0ed
Laura <laurahannah44@gmail.com>
2023-02-26 13:32:29 +0000

    the mod so far (1.5)

---
+++ ConfigMenu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using OptionalUI;
+using UnityEngine;
+
+namespace ThrowMovement
+{
+	public class Config : OptionInterface
+	{
+		public Config() : base(ThrowMovement.instance)
+		{
+		}
+
+		public override bool Configuable()
+		{
+			return true;
+		}
+
+		public override void ConfigOnChange()
+		{
+			ThrowMovement.reqFramesOnGround = int.Parse(OptionInterface.config["D1"]);
+			ThrowMovement.reqFramesSinceThrow = int.Parse(OptionInterface.config["D2"]);
+			ThrowMovement.defRockLifespan = int.Parse(OptionInterface.config["D3"]);
+		}
+
+		public override void Initialize()
+		{
+			base.Initialize();
+			this.Tabs = new OpTab[]
+			{
+				new OpTab("Config")
+			};
+			OpLabel labelAuthor = new OpLabel(20f, 570f, "ThrowMovement mod by pinecubes", true);
+			OpLabel labelPing = new OpLabel(20f, 500f, "laura#2871 for any questions or suggestions!", false);
+			OpLabel labelNote = new OpLabel(200f, 530f, "Gain free rocks on the ground that despawn once thrown!\n(When a rock is in slugcat's stomach)", false);
+
+			int top = 200;
+
+			OpLabel labelGround = new OpLabel(20f, (600 - top), "Grounded time until rocks", false);
+			OpLabel labelGroundUnit = new OpLabel(356f, (600 - top + 2), " frames", false);
+			OpTextBox typerGround = new OpTextBox(new Vector2(300f, (600 - top)), 50, "D1", "0", OpTextBox.Accept.Int)
+			{
+				description = "How many frames should slugcat be on the ground before they get rocks? (40fps)",
+				colorEdge = Color.clear,
+				colorText = new Color(122f, 216f, 255f)
+			};
+			OpLabel labelThrow = new OpLabel(20f, (600 - top - 30), "Throw time until rocks", false);
+			OpLabel labelThrowUnit = new OpLabel(356f, (600 - top - 30 + 2), " frames", false);
+			OpTextBox typerThrow = new OpTextBox(new Vector2(300f, (600 - top - 30)), 50, "D2", "20", OpTextBox.Accept.Int)
+			{
+				description = "At what frame interval after throwing should slugcat get rocks? (40fps)",
+				colorEdge = Color.clear,
+				colorText = new Color(122f, 216f, 255f)
+			};
+			OpLabel labelDespawn = new OpLabel(20f, (600 - top - 60), "Time until despawn", false);
+			OpLabel labelDespawnUnit = new OpLabel(356f, (600 - top - 60 + 2), " frames", false);
+			OpTextBox typerDespawn = new OpTextBox(new Vector2(300f, (600 - top - 60)), 50, "D3", "80", OpTextBox.Accept.Int)
+			{
+				description = "How many frames should rocks be thrown before they despawn? (40fps)",
+				colorEdge = Color.clear,
+				colorText = new Color(122f, 216f, 255f)
+			};
+			this.Tabs[0].AddItems(new UIelement[]
+			{
+				labelAuthor,
+				labelPing,
+				labelNote,
+				labelGround,
+				labelGroundUnit,
+				typerGround,
+				labelThrow,
+				labelThrowUnit,
+				typerThrow,
+				labelDespawn,
+				labelDespawnUnit,
+				typerDespawn
+			});
+		}
+	}
+}
---
+++ ThrowMovement.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OptionalUI;
+using Partiality.Modloader;
+using UnityEngine;
+
+namespace ThrowMovement
+{
+    public class ThrowMovement : PartialityMod
+    {
+        public static OptionInterface LoadOI()
+        {
+            return new Config();
+        }
+
+        public ThrowMovement()
+        {
+            instance = this;
+            this.ModID = "ThrowMovement";
+            this.Version = "1";
+            this.author = "laura#2871";
+        }
+        public static ThrowMovement instance;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            On.Player.ReleaseObject += Player_ReleaseObject;
+            On.Player.ThrowObject += Player_ThrowObject;
+            On.Player.GrabUpdate += Player_GrabUpdate;
+            On.Rock.Update += Rock_Update;
+            On.Rock.PickedUp += Rock_PickedUp;
+        }
+
+        private void Player_ReleaseObject(On.Player.orig_ReleaseObject orig, Player self, int grasp, bool eu)
+        {
+            orig(self, grasp, eu);
+            rockReplenishInterval = reqFramesSinceThrow;
+        }
+
+        private void Player_ThrowObject(On.Player.orig_ThrowObject orig, Player self, int grasp, bool eu)
+        {
+            orig(self, grasp, eu);
+            rockReplenishInterval = reqFramesSinceThrow;
+        }
+
+        private void Player_GrabUpdate(On.Player.orig_GrabUpdate orig, Player self, bool eu)
+        {
+            orig(self, eu);
+            if (self.objectInStomach?.type == AbstractPhysicalObject.AbstractObjectType.Rock)
+            {
+                if (self.lowerBodyFramesOnGround > reqFramesOnGround && self.FreeHand() > -1 && rockReplenishInterval <= 0)
+                {
+                    AbstractPhysicalObject newRock = new AbstractPhysicalObject(self.room.world, AbstractPhysicalObject.AbstractObjectType.Rock, null,
+                                                                                self.room.GetWorldCoordinate(self.firstChunk.pos), self.room.game.GetNewID());
+                    newRock.RealizeInRoom();
+                    self.SlugcatGrab(newRock.realizedObject, self.FreeHand());
+                    rockLifespans.Add(newRock.realizedObject as Rock, defRockLifespan);
+                    rockReplenishInterval = reqFramesSinceThrow;
+                }
+                rockReplenishInterval -= 1;
+            }
+        }
+
+        private void Rock_Update(On.Rock.orig_Update orig, Rock self, bool eu)
+        {
+            orig(self, eu);
+            if (rockLifespans.ContainsKey(self) && !(self.mode is Weapon.Mode.Carried))
+            {
+                if (rockLifespans[self] <= 0) self.Destroy();
+                rockLifespans[self] -= 1;
+            }
+        }
+
+        private void Rock_PickedUp(On.Rock.orig_PickedUp orig, Rock self, Creature upPicker)
+        {
+            if (rockLifespans.ContainsKey(self) && !(self.mode is Weapon.Mode.Carried))
+            {
+                rockLifespans[self] = defRockLifespan;
+            }
+        }
+
+        public static int reqFramesOnGround;
+
+        public static int reqFramesSinceThrow;
+        public static int rockReplenishInterval = 0;
+
+        public static int defRockLifespan;
+        public Dictionary<Rock, int> rockLifespans = new Dictionary<Rock, int>();
+    }
+}
